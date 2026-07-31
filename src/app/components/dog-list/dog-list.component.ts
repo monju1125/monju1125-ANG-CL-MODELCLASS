2ccd4b3117498eedfb4c2b47bc020481fe44c94a
monju1125 <monju1125@github.com>
2026-07-31 15:15:25 +0000

    Test run: 5/5 passed, 0 failed, 0 skipped - 2026-07-31T15:15:25.683Z

--- src/app/components/dog-list/dog-list.component.ts
+++ src/app/components/dog-list/dog-list.component.ts
@@ -29,6 +29,13 @@ export class DogListComponent implements OnInit {
    * you should call the dogsService.addDog(newDog); method.
    */
   addDog(){
+      let newDog: Dog ={
+            name: this.nameInput,
+            breed:this.breedInput,
+            age: this.ageInput
+      };
+
+      this.dogsService.addDog(newDog);
   }
 
   ngOnInit(): void {
